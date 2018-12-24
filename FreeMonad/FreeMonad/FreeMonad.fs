236f37a8fde7233d3acac0e09f351f6ed78cb8a3
Paweł Iżycki <pawel.izycki@britishcouncil.pl>
2018-12-24 14:21:05 +0100

    Define UserRepository interface

--- FreeMonad/FreeMonad/FreeMonad.fs
+++ FreeMonad/FreeMonad/FreeMonad.fs
@@ -1,7 +1,22 @@
 module FreeMonad
+open System
 
 
+type UserId = 
+    UserId of Guid
 
+type CreateUserRequest = {
+    Name: string
+}
+
+type UserEntity = {
+    Id: UserId;
+    Name: string
+}
+
+type IUserRepository =
+    abstract member Create : user:CreateUserRequest -> UserId
+    abstract member Get : userId:UserId -> UserEntity
 
 [<EntryPoint>]
 let main argv =
